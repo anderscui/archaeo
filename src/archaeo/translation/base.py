7614ff5d3cb955b4f1afbad49b49b2c2d580e1a0
andersc <anderscui@gmail.com>
2026-06-10 00:17:28 +0800

    save pdf translation results.

--- src/archaeo/translation/base.py
+++ src/archaeo/translation/base.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Any
 
 from pydantic import BaseModel, Field
+from tqdm import tqdm
 
 from archaeo.chunking.base import Chunk, chunk_pdf
 from archaeo.io.files import json_dump, json_load
@@ -96,7 +97,7 @@ def translate_chunks(
             }
             result = cached
 
-    for chunk in chunks:
+    for chunk in tqdm(chunks, desc=f'Translating (by {provider.name})'):
         if chunk.chunk_id in done and done[chunk.chunk_id].translated_text:
             continue
 
@@ -167,41 +168,81 @@ def translate_pdf_by_chunks(
 
 def translation_result_to_markdown(
     result: TranslationResult,
-    *,
-    include_source: bool = False,
 ) -> str:
     parts = []
-
     for chunk in result.chunks:
-        if include_source:
-            parts.append(f'<!-- chunk_id: {chunk.chunk_id} -->')
-            parts.append('')
-            parts.append('原文：')
-            parts.append('')
-            parts.append(chunk.source_text.strip())
-            parts.append('')
-            parts.append('译文：')
-            parts.append('')
+        translated_text = (chunk.translated_text or '').strip()
+        if translated_text:
+            parts.append(translated_text)
 
-        if chunk.translated_text:
-            parts.append(chunk.translated_text.strip())
-            parts.append('')
-
-    return '\n\n'.join(parts).strip()
+    return '\n\n'.join(parts)
 
 
 def save_translation_markdown(
     result: TranslationResult,
-    output_file: str | Path,
-    *,
-    include_source: bool = False,
+    output_file: str | Path
 ) -> None:
     output_file = Path(output_file)
     output_file.parent.mkdir(parents=True, exist_ok=True)
 
     md = translation_result_to_markdown(
+        result
+    )
+
+    output_file.write_text(md, encoding='utf-8')
+
+
+def translation_result_to_bilingual_markdown(
+    result: TranslationResult,
+    *,
+    source_heading: str = '原文',
+    translation_heading: str = '译文',
+    include_metadata: bool = False,
+) -> str:
+    parts = []
+
+    for i, chunk in enumerate(result.chunks, start=1):
+        # title = chunk.section_title or f'Chunk {i}'
+
+        # parts.append(f'## {i}. {title}')
+        # parts.append('')
+
+        if include_metadata:
+            parts.append(f'<!-- chunk_id: {chunk.chunk_id} -->')
+            parts.append('')
+
+        if source_heading:
+            parts.append(f'### {source_heading}')
+            parts.append('')
+        parts.append(chunk.source_text.strip())
+        parts.append('')
+
+        if translation_heading:
+            parts.append(f'### {translation_heading}')
+            parts.append('')
+        parts.append((chunk.translated_text or '').strip())
+        parts.append('')
+
+        parts.append('---')
+        parts.append('')
+
+    return '\n'.join(parts).strip()
+
+
+def save_bilingual_markdown(
+    result: TranslationResult,
+    output_file: str | Path,
+    *,
+    include_metadata: bool = False,
+) -> None:
+    output_file = Path(output_file)
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+
+    md = translation_result_to_bilingual_markdown(
         result,
-        include_source=include_source,
+        source_heading='',
+        translation_heading='',
+        include_metadata=include_metadata
     )
 
     output_file.write_text(md, encoding='utf-8')
@@ -215,57 +256,61 @@ if __name__ == '__main__':
     # cloud：gemini_flash_lite_3_1 > minimax_m3 = deepseek_v4_pro > qwen3_7_plus
     # local：gemma4:26b > qwen3.5:9b
 
+    # llm = OllamaProvider('gemma4:e2b')  # 1.1, 2.4, 10.2 -> 忽略
+    # llm = OllamaProvider('gemma4:e2b-it-qat')  # 1.1, 2.4, 9.9 -> 忽略
     # llm = OllamaProvider('gemma4:12b')  # 0.65, 8.5, 42.0 -> 忽略
-    # llm = OllamaProvider('gemma4:26b')  # 0.5, 4.1, 19.5
-    llm = OllamaProvider('qwen3.5:0.8b')  # 0.3, 1.5, 5.7
     # llm = OllamaProvider('qwen3.5:2b')  # 0.3, 2.5, 10.5 -> 忽略
     # llm = OllamaProvider('qwen3.5:4b')  # 0.5, 3.9, 17.9 -> 忽略
-    # llm = OllamaProvider('qwen3.5:9b')  # 0.63, 5.9, 25.9 -> 基本忽略
     # llm = OllamaProvider('qwen3.5:9b-nvfp4')  # 0.5, 5.3, 25.5 -> 忽略（速度提升很有限，依然很慢）
-    # llm = OpenRouterProvider(OpenRouterModels.gemini_flash_lite_3_1)  # 1.7, 2.2, 5.7
     # llm = OpenRouterProvider(OpenRouterModels.gemini_flash_lite_2_5)  # 1.3, 2.2, 6.6 -> 忽略
     # llm = OpenRouterProvider(OpenRouterModels.gpt_5_4_nano)  # 3.2, 4.3, 12.5 -> 忽略
+
+    # llm = OllamaProvider('qwen3.5:9b')  # 0.63, 5.9, 25.9 -> 基本忽略
     # llm = OpenRouterProvider(OpenRouterModels.gpt_5_4_mini)  # 1.4, 3.1, 12.5 -> 普通
-    # llm = OpenRouterProvider(OpenRouterModels.qwen3_7_plus)  # 2.3, 7.6, 20.1 -> 普通
     # llm = OpenRouterProvider(OpenRouterModels.deepseek_v4_flash)  # 3.3, 7.1, 17.1 -> 普通
+    # llm = OpenRouterProvider(OpenRouterModels.qwen3_7_plus)  # 2.3, 7.6, 20.1 -> 普通
+
+    # llm = OllamaProvider('qwen3.5:0.8b')  # 0.3, 1.5, 5.7
+    llm = OllamaProvider('gemma4:26b')  # 0.5, 4.1, 19.5
+    # llm = OpenRouterProvider(OpenRouterModels.gemini_flash_lite_3_1)  # 1.7, 2.2, 5.7
     # llm = OpenRouterProvider(OpenRouterModels.deepseek_v4_pro)  # 3.7, 6.6, 26.2
     # llm = OpenRouterProvider(OpenRouterModels.minimax_m3)  # 3.3, 7.5, 34.7
 
-    start = time.time()
-    print(translate_text('hello, world', llm))
-    print(f'time elapsed: {time.time() - start}')
-
-    text = """COCKTAIL PARTIES have seen worse pickup attempts. In 1989 Andrew
-    Wylie, a literary agent, set his sights on Philip Roth. (“Every time I turned
-    around, there was this guy,” the novelist later said. “I discovered what it was
-    like to be a pretty girl.”) When Mr Wylie made his move, he declared that he
-    could get Roth a three-book deal worth $2m.
-
-    Later, over lunch, he formally wooed him. Roth was underappreciated by his
-    publisher, Mr Wylie asserted, and his potentially lucrative foreign rights
-    were being neglected. He could handle the rights to Roth’s 14 novels in 30
-    territories, which amounted to 420 contracts renegotiated every seven years.
-    Roth’s revenue could increase by up to 500%. It was an offer the writer--
-    who had a mighty reputation but a puny bank balance—could not refuse."""
-    start = time.time()
-    print(translate_text(text, llm))
-    print(f'time elapsed: {time.time() - start}')
-
-    text = """The hidden tastemakers of the literary world
-    Literary agents are more important than readers give them credit for
-    June 4th 2026
-
-    COCKTAIL PARTIES have seen worse pickup attempts. In 1989 Andrew Wylie, a literary agent, set his sights on Philip Roth. (“Every time I turned around, there was this guy,” the novelist later said. “I discovered what it was like to be a pretty girl.”) When Mr Wylie made his move, he declared that he could get Roth a three-book deal worth $2m.
-    Later, over lunch, he formally wooed him. Roth was underappreciated by his publisher, Mr Wylie asserted, and his potentially lucrative foreign rights were being neglected. He could handle the rights to Roth’s 14 novels in 30 territories, which amounted to 420 contracts renegotiated every seven years. Roth’s revenue could increase by up to 500%. It was an offer the writerwho had a mighty reputation but a puny bank balance—could not refuse. Mr Wylie is famous, infamous even, in the publishing world: he earned the nickname “the Jackal”, after persuading several authors to ditch their agents and join him. When he wanted to expand his network of contacts in China, he went so far as to call Henry Kissinger to suggest that he could become his agent and that Kissinger should write a book on China. That book went on to sell well, and Mr Wylie gained footing and a stronger Rolodex for future business there. Mr Wylie’s strategy of focusing on foreign rights and sales “not only changed the fortunes of the writers on his list, but the shape of American literature abroad”, writes Laura McGrath, an assistant professor of English at Temple University, in a new survey of literary agents.
-    Agents, Ms McGrath argues, have many functions. They are spotters, identifying promising writers. They are confidants, helping those writers to produce their best work. And they are negotiators, doing deals on behalf of authors and getting them more money so they can have more time to write.
-    That means agents are often controversial figures within the publishing world. In 1897 a British publisher described agents as “generally a parasite”. (They take a commission of around 15% of an author’s earnings.) They retain an aura of mystery. The literary agent’s job is to “be invisible”, Ms McGrath contends, working behind the scenes to create a star and thereby shape what gets written and what people read.
-    “Middlemen” is one of a pile of recent books offering a behind-the-scenes account of how literature comes to people’s shelves. Ms McGrath is a lively writer, eschewing academic jargon in favour of anecdotes. She heads to the world’s largest book fair in Frankfurt. She watches a group of agents evaluate the unsolicited manuscripts in the “slush pile”. (Every agent’s dream is to find a “Harry Potter”-like gem among the dross, as J.K. Rowling’s agent did.) She describes the evolution of the “publishing lunch”: Candida Donadio, who found Joseph Heller in the slush pile, once joked that she took three lunches in a day. (Today there are fewer stiff drinks and more talk about limp sales.)
-    Agents try to devise creative answers to marketing problems. For example, how do you sell a book by an unknown writer? Sterling Lord, Jack Kerouac’s agent, started a trend when he worked with publishers to build hype around “On the Road”; they advertised Kerouac as “the voice of a new age”. Agents ever since have hoped their debut novelists can make waves: first novels now account for between 15% and 25% of the fiction published every year. Many of these books fetch high advances that sales cannot recoup. Most American novelists now publish only one title.
-    Literary agents are never objective, and their personal preferences shape what is printed. Because most agents and editors live in New York, in 200022 more novels were set there than in the next 30 most populous American cities combined. And certainly the proliferation of agents—there are now more than 1,500 in America—has something to do with the proliferation of mediocre books. Many should have never have been printed. But what is striking, at a time when taste is increasingly shaped by algorithms, is just how much an individual agent’s judgment still matters."""
-
-    start = time.time()
-    print(translate_text(text, llm))
-    print(f'time elapsed: {time.time() - start}')
+    # start = time.time()
+    # print(translate_text('hello, world', llm))
+    # print(f'time elapsed: {time.time() - start}')
+    #
+    # text = """COCKTAIL PARTIES have seen worse pickup attempts. In 1989 Andrew
+    # Wylie, a literary agent, set his sights on Philip Roth. (“Every time I turned
+    # around, there was this guy,” the novelist later said. “I discovered what it was
+    # like to be a pretty girl.”) When Mr Wylie made his move, he declared that he
+    # could get Roth a three-book deal worth $2m.
+    #
+    # Later, over lunch, he formally wooed him. Roth was underappreciated by his
+    # publisher, Mr Wylie asserted, and his potentially lucrative foreign rights
+    # were being neglected. He could handle the rights to Roth’s 14 novels in 30
+    # territories, which amounted to 420 contracts renegotiated every seven years.
+    # Roth’s revenue could increase by up to 500%. It was an offer the writer--
+    # who had a mighty reputation but a puny bank balance—could not refuse."""
+    # start = time.time()
+    # print(translate_text(text, llm))
+    # print(f'time elapsed: {time.time() - start}')
+    #
+    # text = """The hidden tastemakers of the literary world
+    # Literary agents are more important than readers give them credit for
+    # June 4th 2026
+    #
+    # COCKTAIL PARTIES have seen worse pickup attempts. In 1989 Andrew Wylie, a literary agent, set his sights on Philip Roth. (“Every time I turned around, there was this guy,” the novelist later said. “I discovered what it was like to be a pretty girl.”) When Mr Wylie made his move, he declared that he could get Roth a three-book deal worth $2m.
+    # Later, over lunch, he formally wooed him. Roth was underappreciated by his publisher, Mr Wylie asserted, and his potentially lucrative foreign rights were being neglected. He could handle the rights to Roth’s 14 novels in 30 territories, which amounted to 420 contracts renegotiated every seven years. Roth’s revenue could increase by up to 500%. It was an offer the writerwho had a mighty reputation but a puny bank balance—could not refuse. Mr Wylie is famous, infamous even, in the publishing world: he earned the nickname “the Jackal”, after persuading several authors to ditch their agents and join him. When he wanted to expand his network of contacts in China, he went so far as to call Henry Kissinger to suggest that he could become his agent and that Kissinger should write a book on China. That book went on to sell well, and Mr Wylie gained footing and a stronger Rolodex for future business there. Mr Wylie’s strategy of focusing on foreign rights and sales “not only changed the fortunes of the writers on his list, but the shape of American literature abroad”, writes Laura McGrath, an assistant professor of English at Temple University, in a new survey of literary agents.
+    # Agents, Ms McGrath argues, have many functions. They are spotters, identifying promising writers. They are confidants, helping those writers to produce their best work. And they are negotiators, doing deals on behalf of authors and getting them more money so they can have more time to write.
+    # That means agents are often controversial figures within the publishing world. In 1897 a British publisher described agents as “generally a parasite”. (They take a commission of around 15% of an author’s earnings.) They retain an aura of mystery. The literary agent’s job is to “be invisible”, Ms McGrath contends, working behind the scenes to create a star and thereby shape what gets written and what people read.
+    # “Middlemen” is one of a pile of recent books offering a behind-the-scenes account of how literature comes to people’s shelves. Ms McGrath is a lively writer, eschewing academic jargon in favour of anecdotes. She heads to the world’s largest book fair in Frankfurt. She watches a group of agents evaluate the unsolicited manuscripts in the “slush pile”. (Every agent’s dream is to find a “Harry Potter”-like gem among the dross, as J.K. Rowling’s agent did.) She describes the evolution of the “publishing lunch”: Candida Donadio, who found Joseph Heller in the slush pile, once joked that she took three lunches in a day. (Today there are fewer stiff drinks and more talk about limp sales.)
+    # Agents try to devise creative answers to marketing problems. For example, how do you sell a book by an unknown writer? Sterling Lord, Jack Kerouac’s agent, started a trend when he worked with publishers to build hype around “On the Road”; they advertised Kerouac as “the voice of a new age”. Agents ever since have hoped their debut novelists can make waves: first novels now account for between 15% and 25% of the fiction published every year. Many of these books fetch high advances that sales cannot recoup. Most American novelists now publish only one title.
+    # Literary agents are never objective, and their personal preferences shape what is printed. Because most agents and editors live in New York, in 200022 more novels were set there than in the next 30 most populous American cities combined. And certainly the proliferation of agents—there are now more than 1,500 in America—has something to do with the proliferation of mediocre books. Many should have never have been printed. But what is striking, at a time when taste is increasingly shaped by algorithms, is just how much an individual agent’s judgment still matters."""
+    #
+    # start = time.time()
+    # print(translate_text(text, llm))
+    # print(f'time elapsed: {time.time() - start}')
 
     # text = """China’s delivery drivers are its most obvious underclass
     #
@@ -294,3 +339,15 @@ if __name__ == '__main__':
     # start = time.time()
     # print(translate_text(text, llm))
     # print(f'time elapsed: {time.time() - start}')
+
+    start = time.time()
+    # pdf_file = '/Users/andersc/Downloads/A Comprehensive Survey on Vector Database - Storage and Retrieval Technique, Challenge (2026.03).pdf'
+    # pdf_file = '/Users/andersc/Downloads/AI research interviews (2025.08).pdf'
+    pdf_file = '/Users/andersc/data/dev/local_kb/new_yorker.2026.06.08.pdf'
+    result = translate_pdf_by_chunks(pdf_file, llm,
+                                     max_tokens=2000,
+                                     n_pages=100,
+                                     cache_file='/Users/andersc/Downloads/new_yorker.2026.06.08.json')
+    save_translation_markdown(result, output_file='/Users/andersc/Downloads/new_yorker.2026.06.08.md')
+    # save_bilingual_markdown(result, output_file='/Users/andersc/Downloads/ai interview1-3.md')
+    print(f'time elapsed: {time.time() - start}')
